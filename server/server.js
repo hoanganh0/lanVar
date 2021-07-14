56e3a2978fd025f2d37b545e803627f649114ffc
hoanganh <hoanganh@sentius.com.au>
2021-07-14 14:55:25 +0700

    updated

--- server/server.js
+++ server/server.js
@@ -3,12 +3,20 @@ const productRoutes = require('./routes/productRoutes');
 const accountRoutes = require('./routes/accountRouters');
 const connectDB = require('./config/db');
 const cors = require('cors');
-const port = 5000;
+const path = require('path');
+const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(express.json());
 
